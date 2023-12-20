688f1fb605e9f7da2a99eeb7f4b8c0c4a5c1122e
saeid rahimi <saeid.rah.1381@gmail.com>
2023-12-20 17:16:16 +0330

    panic exercises

---
+++ panic.go
@@ -0,0 +1,17 @@
+// panic
+package main
+
+import "fmt"
+
+func main() {
+
+	a := []string{"a", "b"}
+	checkAndPrint(a, 2)
+}
+
+func checkAndPrint(a []string, index int) {
+	if index > (len(a) - 1) {
+		panic("Out of bound access for slice")
+	}
+	fmt.Println(a[index])
+}
